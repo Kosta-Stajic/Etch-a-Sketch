e7d2c481c1626182b4e3099b37fb68cfa062f830
Kosta-Stajic <kostastajic@gmail.com>
2023-06-07 16:33:11 +0200

    Add random RGB button

--- scrypt.js
+++ scrypt.js
@@ -4,25 +4,36 @@ const container = document.querySelector(".container");
 const clear = document.querySelector(".newGrid")
 container.style.height = "600px"
 container.style.width = "600px"
+const rainbow = document.querySelector(".RGB")
+
+const colors = ["red", "blue", "yellow","green","orange","purple","indigo","pink", "crimson", "CadetBlue", "Cyan", "DarkGoldenRod"]
+
+function getRandomColor(){
+    return colors[Math.floor(Math.random()*colors.length)]
+} 
+console.log(getRandomColor())
 
 function generateGrid(x) {
     for (let i = 0; i < x * x; i++) {
-        const row = document.createElement("div");
-        let size = 600/x;
-        console.log(size)
+        let row = document.createElement("div");
+        let size = 600 / x;
         row.classList.add("cells")
         row.style.height = `${size}px`
         row.style.width = `${size}px`
         container.appendChild(row)
-        console.log (row.style.height)
+       
         row.addEventListener("mouseover", () => {
             row.style.background = "grey";
         })
-
+        rainbow.addEventListener("click", () =>
+        row.addEventListener("mouseover", () => {
+            row.style.background = getRandomColor();
+        }))
     }
 }
 
-clear.addEventListener ("click", () => {
+
+clear.addEventListener("click", () => {
     let userInput = parseInt(prompt("Chose a size"))
     container.textContent = ""
     generateGrid(userInput)
