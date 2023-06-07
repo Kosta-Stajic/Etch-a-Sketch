a59fa343b80bde080c0eb86350f562547d25eea3
Kosta-Stajic <kostastajic@gmail.com>
2023-06-07 12:36:50 +0200

    Add dynamic height/width + choice of grid size

--- scrypt.js
+++ scrypt.js
@@ -1,20 +1,38 @@
+
+
 const container = document.querySelector(".container");
+const clear = document.querySelector(".newGrid")
+container.style.height = "600px"
+container.style.width = "600px"
+
+function generateGrid(x) {
+    for (let i = 0; i < x * x; i++) {
+        const row = document.createElement("div");
+        let size = 600/x;
+        console.log(size)
+        row.classList.add("cells")
+        row.style.height = `${size}px`
+        row.style.width = `${size}px`
+        container.appendChild(row)
+        console.log (row.style.height)
+        row.addEventListener("mouseover", () => {
+            row.style.background = "grey";
+        })
+
+    }
+}
+
+clear.addEventListener ("click", () => {
+    let userInput = parseInt(prompt("Chose a size"))
+    container.textContent = ""
+    generateGrid(userInput)
+
+})
 
 
-function generateGrid(x){
-for (let i=0; i < x; i++){
-    const row = document.createElement("div");
-    row.classList.add ("row")
-    container.appendChild (row)
-    row.addEventListener("mouseover",() => {
-        row.style.background = "grey";
-
-    })
-    
-}}
-
-generateGrid(256);
+generateGrid(16);
 
 
-const newGrid = document.getElementsByClassName("btn");
+
+
 
